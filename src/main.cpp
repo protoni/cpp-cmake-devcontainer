bce599234f03ced86a94553a6ea9839219bda80f
Toni <toni@protoni.fi>
2023-10-01 19:32:37 +0300

    Add clang-formatter with modified google style rules

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,31 @@
 #include <iostream>
 #include <string>
 
-struct CommandLineParseStatus
-{
-    enum class Code {
-        Init,
-        Ok,
-        Error,
-        VersionRequested,
-        HelpRequested
-    };
-    
+struct CommandLineParseStatus {
+    enum class Code { Init, Ok, Error, VersionRequested, HelpRequested };
+
     Code code;
     std::string error;
-    
+
     explicit CommandLineParseStatus(Code status, const std::string& err = "")
-        : code(status)
-        , error(err) {}
+        : code(status), error(err) {}
 };
 
-CommandLineParseStatus parse(int opt)
-{
+CommandLineParseStatus parse(int opt) {
     using Code = CommandLineParseStatus::Code;
-    
+
     if (opt == 0)
         return CommandLineParseStatus(Code::Init);
-        
+
     if (opt == 1)
         return CommandLineParseStatus(Code::Ok);
-        
+
     return CommandLineParseStatus(Code::Error);
 }
 
-int main()
-{
-  CommandLineParseStatus status = parse(0);
-    
-  if (status.code == CommandLineParseStatus::Code::Init)
-     std::cout << "OK" << std::endl;
+int main() {
+    CommandLineParseStatus status = parse(0);
 
+    if (status.code == CommandLineParseStatus::Code::Init)
+        std::cout << "OK" << std::endl;
 }
